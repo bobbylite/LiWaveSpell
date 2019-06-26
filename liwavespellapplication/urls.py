46aff964966552d7f8afc04e031505fb302abec9
Robert Luisi <bobby.luisi@gmail.com>
2019-06-26 10:32:34 -0400

    Created seperate module for spot card

--- liwavespellapplication/urls.py
+++ liwavespellapplication/urls.py
@@ -3,5 +3,5 @@ from liwavespellapplication.views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='liwavespell-home'),
-    path('/<str:theme>/', HomeView.as_view(), name='liwavespell-home-theme'),
+    path('<str:theme>/', HomeView.as_view(), name='liwavespell-home-theme'),
 ]
--- liwavespellproject/urls.py
+++ liwavespellproject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('waves', include('liwavespellapplication.urls'))
+    path('waves/', include('liwavespellapplication.urls'))
 ]
